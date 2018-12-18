e8f8f20b34a1a31bc66df268b38fb2449ee33ff1
Sergey Sklyarov <sergey.sklyarov@gmail.com>
2018-12-18 15:59:41 +0200

    Initial commit

---
+++ BubbleSort.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+
+            for (int y = 0; y < arr.Length; y++)
+            {
+                for (int i = 0; i < arr.Length - 1; i++)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        var tmp = arr[i + 1];
+                        arr[i + 1] = arr[i];
+                        arr[i] = tmp;
+                    }
+                }
+
+                Console.WriteLine(string.Join(",", arr));
+            }
+        }
+    }
+}
